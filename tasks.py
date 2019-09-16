acfc616b9c6b5bc67e6f724e407b77545a0dd996
Damien Flament <damien.flament@gmx.com>
2019-09-16 12:22:18 +0200

    Use pre-commit framework.

--- tasks.py
+++ tasks.py
@@ -24,18 +24,6 @@ def get_module_doc():
 
 
 @task
-def lint(c):
-    """Lint the source code"""
-    c.run("flake8", hide="out")
-
-
-@task(name="quick-test")
-def quick_test(c):
-    """Run quick tests"""
-    c.run("pytest", hide="out")
-
-
-@task()
 def doc(c):
     """Generates the documentation"""
     module = get_module_doc()
@@ -53,11 +41,3 @@ def readme(c):
 
     README_TARGET.touch()
     README_TARGET.write_text("\n".join(["temptree", "========", module.docstring]))
-
-
-@task(lint, quick_test, doc, readme, name="pre-commit")
-def pre_commit(c):
-    """Run tasks required for commiting"""
-    c.run(f"git add {DOCS_TARGET}")
-    c.run(f"git add {NOJEKYLL_TARGET}")
-    c.run(f"git add {README_TARGET}")
